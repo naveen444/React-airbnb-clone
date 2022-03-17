094d51f7b16661be629454d44ab6db8ba4bbe549
naveen <naveen.bunty444@gmail.com>
2022-03-17 18:51:44 +0530

    edited NavBar(added navs and css)

--- src/utility/NavBar/NavBar.js
+++ src/utility/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './NavBar.css';
+import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
   constructor() {
@@ -7,8 +8,31 @@ class NavBar extends Component {
   }
 
   render() {
+
+    let navColor = 'transparent'
+    if (this.props.location.pathname !== '/') {
+        //   user is on the home page
+        navColor = 'black'
+    }
+
     return (
-      <h1>Sanity NavBar</h1>
+    <div className="container-fluid nav">
+        <div className="row">
+            <nav className={navColor}>
+                <div className="nav-wrapper">
+                    <Link to = "/" className="left">airbnb</Link>
+                    <ul id="nav-mobile" className="right">
+                        <li><Link to="/">English (US)</Link></li>
+                        <li><Link to="/">$ USD</Link></li>
+                        <li><Link to="/">Become a host</Link></li>
+                        <li><Link to="/">Help</Link></li>
+                        <li><Link to="/">Sign Up</Link></li>
+                        <li><Link to="/">Log In</Link></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
     )
   }
 }
