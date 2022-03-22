ba34bcacaeb2a143aba96f9c03a0cb3310dd1969
naveen <naveen.bunty444@gmail.com>
2022-03-23 00:00:33 +0530

    added Modal component on Login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import SingleFullVenue from "./pages/SingleFullVenue/SingleFullVenue";
 import NavBar from "./utility/NavBar/NavBar";
+import Modal from "./utility/Modal/Modal";
 // import './App.css';
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
         <Route path = "/" component={NavBar} />
         <Route exact path = "/" component={Home} />
         <Route exact path="/venue/:vid" component={SingleFullVenue} />
+        <Route path="/" component={Modal} />
       </Router>
     )
   }
--- src/utility/NavBar/NavBar.js
+++ src/utility/NavBar/NavBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import './NavBar.css';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import openModal from "../../actions/openModal";
 
 class NavBar extends Component {
   constructor() {
@@ -27,7 +30,7 @@ class NavBar extends Component {
                         <li><Link to="/">Become a host</Link></li>
                         <li><Link to="/">Help</Link></li>
                         <li><Link to="/">Sign Up</Link></li>
-                        <li><Link to="/">Log In</Link></li>
+                        <li onClick={()=>{this.props.openModal('open',"Login")}}>Log In</li>
                     </ul>
                 </div>
             </nav>
@@ -37,4 +40,10 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar;
+function mapDispatchToProps(dispatcher) {
+  return bindActionCreators({
+    openModal: openModal
+  },dispatcher)
+}
+
+export default connect(null, mapDispatchToProps)(NavBar);
